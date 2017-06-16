abcfcd97a9465e272599b02fc441a468741e9eb2
lucas niewohner <lucasniewohner@outlook.com>
2017-06-15 20:04:34 -0500

    Created a NavMesh.  Keep in mind that the NavMesh itself is heavily dependant on the actual Scene map, so the one in the commit is kinda useless.  Also, NavMesh Agents are effectively useless without the NavMesh,which discounts that portion of this commit.  The only reuseable part of the commit is the MoveTo.cs script, which is about 30 seconds of work.

---
+++ KoboldKing/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+    public Transform Goal;
+
+    private NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        navMeshAgent.SetDestination(Goal.position);
+	}
+}
