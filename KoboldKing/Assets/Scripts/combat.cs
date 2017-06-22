6857b029e4d7aaa7c56481320f7c2284cbe18b36
saingledue <saingledue@outlook.com>
2017-06-22 18:28:51 -0500

    House Keeping!

---
+++ KoboldKing/Assets/Scripts/combat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combat : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Console.WriteLine("Youve hit me");
+        CombatdFire();
+    }
+    void CombatdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Spawn the bullet on the Clients
+        // NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
