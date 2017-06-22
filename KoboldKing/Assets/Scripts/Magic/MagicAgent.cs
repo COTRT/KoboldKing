3059393833bffdb59f9045cadf1259cb72ec1dd3
Hermanoid <lucasniewohner@gmail.com>
2017-06-22 17:09:43 -0500

    Work-In-Progress Magic System

---
+++ KoboldKing/Assets/Scripts/Magic/MagicAgent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts.Managers;
+using System.Linq;
+
+public class MagicAgent : MonoBehaviour
+{
+    public string spellName;
+    public HashSet<string> AvailableSpells;
+
+    private MagicManager magicManager;
+    // Use this for initialization
+    void Start()
+    {
+        AvailableSpells = new HashSet<string>();
+        magicManager = this.GetManager<MagicManager>();
+        foreach (var spell in magicManager.Spells.Keys)
+        {
+            AvailableSpells.Add(spell);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            TryCast(spellName);
+        }
+    }
+
+    /// <summary>
+    /// Not type casting - spell casting
+    /// </summary>
+    bool TryCast(string spell)
+    {
+        bool canCast = AvailableSpells.Contains(spell) && magicManager.Exists(spell);
+        if (canCast)
+        {
+            magicManager[spell].Cast(gameObject);
+        }
+        return canCast;
+    }
+}
---
+++ KoboldKing/Assets/Scripts/Magic/Spells/Spell.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Spell : MonoBehaviour
+{
+    ParticleSystem partSys;
+    MeshRenderer meshRend;
+    //ParticleSystem particleSystem;
+    // Use this for initialization
+    void Start()
+    {
+        partSys = GetComponent<ParticleSystem>();
+        meshRend = GetComponent<MeshRenderer>();
+        if (meshRend != null)
+        {
+            meshRend.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Cast(GameObject Caster)
+    {
+        transform.position = Caster.transform.position;
+        if (meshRend != null)
+        {
+            meshRend.enabled = false;
+        }
+        
+        partSys.Play();
+    }
+}
