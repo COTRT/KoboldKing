e75e2025b80f1a67c334b521ecaf5a8e99ecc736
lucas niewohner <lucasniewohner@outlook.com>
2017-06-23 08:31:08 -0500

    Merge remote-tracking branch 'origin/Development' into #0037_SpellSystem
    (cherry picked from commit 3c7cb18c905e322d6f20f1297ed2ce5f661ae9c2)
    
    # Conflicts:
    #	KoboldKing/Assets/Fonts.meta
    #	KoboldKing/Assets/Materials/Kobold Claw.mat.meta
    #	KoboldKing/Assets/Materials/Red.mat.meta
    #	KoboldKing/Assets/Materials/unnamed.mat.meta
    #	KoboldKing/Assets/Prefabs/Health/Bullet.prefab.meta
    #	KoboldKing/Assets/Prefabs/Health/Enemy.prefab.meta
    #	KoboldKing/Assets/Prefabs/Health/GameController.prefab.meta
    #	KoboldKing/Assets/Prefabs/Health/Player.prefab.meta
    #	KoboldKing/Assets/Prefabs/Materials.meta
    #	KoboldKing/Assets/Prefabs/Terrain.prefab.meta
    #	KoboldKing/Assets/Resources/Effects.meta
    #	KoboldKing/Assets/Scenes.meta~origin_#0037_SpellSystem
    #	KoboldKing/Assets/Scenes/BirthCaveSystem.unity
    #	KoboldKing/Assets/Scenes/HeathTesting.unity
    #	KoboldKing/Assets/Scenes/NavMeshTest.unity.meta
    #	KoboldKing/Assets/Scripts/ButtonContoller.cs.meta
    #	KoboldKing/Assets/Scripts/CameraFollow.cs.meta
    #	KoboldKing/Assets/Scripts/Devices/DeviceTrigger.cs.meta
    #	KoboldKing/Assets/Scripts/Follow.cs
    #	KoboldKing/Assets/Scripts/Follow.cs.meta
    #	KoboldKing/Assets/Scripts/Health.meta
    #	KoboldKing/Assets/Scripts/Health/Bullet.cs.meta
    #	KoboldKing/Assets/Scripts/Health/FPSInput.cs.meta
    #	KoboldKing/Assets/Scripts/MainMenu.cs.meta
    #	KoboldKing/Assets/Terrain.meta

--- KoboldKing/Assets/Scripts/Follow.cs
+++ KoboldKing/Assets/Scripts/Follow.cs
@@ -2,21 +2,20 @@
 using System.Collections;
 
 public class Follow : MonoBehaviour {
-    Transform tr_Player;
+
+		Transform tr_Player;
 		float f_RotSpeed=3.0f,f_MoveSpeed = 4.0f;
 
 
 		void Start () {
 
-            tr_Player = GameObject.FindGameObjectWithTag ("Player").transform; }
-            
+			tr_Player = GameObject.FindGameObjectWithTag ("Player").transform; }
+
 
 		void Update () {
-        if (tr_Player != null) { 
-            //This is to look at the player object, and the second one is to go to the player object.
-            transform.rotation = Quaternion.Slerp (transform.rotation , Quaternion.LookRotation (tr_Player.position - transform.position) , f_RotSpeed * Time.deltaTime);
+        //This is to look at the player object, and the second one is to go to the player object.
+			transform.rotation = Quaternion.Slerp (transform.rotation , Quaternion.LookRotation (tr_Player.position - transform.position) , f_RotSpeed * Time.deltaTime);
 			transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
-            }
 		}
 	}
 
