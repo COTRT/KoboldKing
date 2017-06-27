935120316b69cb00d3b61598ad9a18d37a606d1d
saingledue <saingledue@outlook.com>
2017-06-26 21:09:39 -0500

    Fixed the camera follow

--- KoboldKing/Assets/Scripts/CameraFollow.cs
+++ KoboldKing/Assets/Scripts/CameraFollow.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
 
-	// Use this for initialization
-	void Start () {
-        if (player != null)
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
-	}
-	
-	// Update is called once per frame after update
-	void LateUpdate () {
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame after update
+    void LateUpdate()
+    {
         if (player != null)
         {
             transform.position = player.transform.position;
         }
-	}
+    }
 }
+
