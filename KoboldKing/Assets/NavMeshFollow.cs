4a5d83ec8f9d58f26773d6fd5da718f98c6e157e
saingledue <saingledue@outlook.com>
2017-06-28 17:25:06 -0500

    Merge branch 'Development' into #52nav_Mesh
    # Conflicts:
    #	KoboldKing/Assets/Scripts/CameraFollow.cs

---
+++ KoboldKing/Assets/NavMeshFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class NavMeshFollow : MonoBehaviour {
+    public Transform target;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //agent.GetComponent<NavMeshFollow>().destination = target.transform.position;
+        agent.SetDestination(target.position);
+    }
+}
