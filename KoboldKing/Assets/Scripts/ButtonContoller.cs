025381a3b73c21cd3f0d4a95de185f7d421f09c8
landenf <landenfogle@gmail.com>
2017-06-30 10:43:08 -0500

    #68 connecting the main menu button on pause menu.

---
+++ KoboldKing/Assets/Scripts/ButtonContoller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonContoller : MonoBehaviour {
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+
+}
