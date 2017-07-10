4c602d9e753d629ddbc7ed4b8638765eafd8a2b2
saingledue <saingledue@outlook.com>
2017-07-10 13:25:42 -0500

    On startup made the player and the enemy appear

---
+++ KoboldKing/Assets/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUp : MonoBehaviour {
+    public GameObject Player;
+    public GameObject Enemy;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(Player);
+        Instantiate(Enemy);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
