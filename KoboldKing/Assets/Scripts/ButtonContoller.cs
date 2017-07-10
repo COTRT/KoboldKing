b9c0ce40268289b549ae035b65f90bf796631c6f
landenf <landenfogle@gmail.com>
2017-07-10 17:46:42 -0500

    I added the button script for die menu for main menu

---
+++ KoboldKing/Assets/Scripts/ButtonContoller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonContoller : MonoBehaviour {
+
+    public GameObject Canvas;
+
+   public void MainMenu()
+    {
+        Application.LoadLevel(0);
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+
+    }
+    public void GoToGame()
+    {
+        Application.LoadLevel(1);
+
+    }
+    public void Resume()
+    {
+        Canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+}
