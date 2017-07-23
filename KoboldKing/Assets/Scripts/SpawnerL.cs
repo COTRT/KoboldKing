92cb06b451a7544635f73fcf2251dbf44591f996
landenf <landenfogle@gmail.com>
2017-07-22 19:51:11 -0500

    updated version

--- KoboldKing/Assets/Scripts/SpawnerL.cs
+++ KoboldKing/Assets/Scripts/SpawnerL.cs
@@ -10,15 +10,18 @@ public class SpawnerL : MonoBehaviour {
     public GameObject Spawns;
 
 
+
     //this is just for testing porposes will be deleted when we are ready to call this function elsewhere.
     void Start() {
         SpawnEnemy();
 
+
     }
+
 
     void SpawnEnemy()
     {
-       Spawns = Instantiate(Enemy, transformLocation.position, Quaternion.Euler(0,0,0)) as GameObject;
+            Spawns = Instantiate(Enemy, transformLocation.position, Quaternion.Euler(0, 0, 0)) as GameObject;
 
     }
 }
