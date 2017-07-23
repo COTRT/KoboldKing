dd3a1281ee9f0632452a66408d516721bb00ab9f
saingledue <saingledue@outlook.com>
2017-07-22 20:09:42 -0500

    Reformated the SpawnSystem

---
+++ KoboldKing/Assets/Encounter1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encounter1 : MonoBehaviour {
+    public GameObject Ecounter1Spawn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(Ecounter1Spawn);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
