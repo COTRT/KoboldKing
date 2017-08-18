2731549256adfe4f21afee652d949c9b8d0a1978
landenf <landenfogle@gmail.com>
2017-08-17 20:27:34 -0500

    Added Encounters to start up script. Encounters are not in the scene.

--- KoboldKing/Assets/Scripts/StartUp.cs
+++ KoboldKing/Assets/Scripts/StartUp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StartUp : MonoBehaviour {
+    public GameObject[] Encounters;
     public GameObject Player;
     public GameObject Enemy;
 
@@ -10,9 +11,17 @@ public class StartUp : MonoBehaviour {
     void Start () {
         Instantiate(Player);
         Instantiate(Enemy);
+        InstantiateAllEncounters();
 
 
 
+    }
+    void InstantiateAllEncounters()
+    {
+        for(int i = 0; i < Encounters.Length; i++)
+        {
+            Instantiate(Encounters[i]);
+        }
     }
 
     // Update is called once per frame
