f7c0359468ed1105771eb06f75cf83c0538977d8
landenf <landenfogle@gmail.com>
2017-09-14 20:26:49 -0500

    Have music in place. Need to be able to change them now.

---
+++ KoboldKing/Assets/MusicTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour {
+    GameObject EventManager;
+    AudioClip Music;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            EventManager = GameObject.FindGameObjectWithTag("Event Manager");
+            AudioSource audioSource = EventManager.GetComponent<AudioSource>();
+            AudioClip audioClip = audioSource.audio;
+            audioClip = Music;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
