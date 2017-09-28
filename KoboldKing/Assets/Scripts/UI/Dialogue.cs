f9e29240a20e1803b9dcabbd323883848f354e5b
AgntBluSqurrl <zed@zedmain.com>
2017-09-28 18:34:07 -0500

    WIP!!!  (mid-way UI)

---
+++ KoboldKing/Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour {
+    private GameObject Player;
+    public Transform Canvas;
+    public bool present = false;
+    public Transform Canvas2;
+
+    void OnTriggerEnter (Collider other) {
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(Canvas);
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                if (present == false)
+                {
+                    Instantiate(Canvas2);
+                    present = true;
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    DestroyObject(GameObject.FindGameObjectWithTag("PauseMenu"));
+                    present = false;
+                    Time.timeScale = 1;
+                }
+            }
+        }
+
+    }
+}
