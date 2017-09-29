05c27295c8aa9114c44177331a85d537a0c434be
landenf <landenfogle@gmail.com>
2017-09-28 20:18:12 -0500

    Added the scene, added some scripts, and edited those scripts.

---
+++ KoboldKing/Assets/Scripts/Inventory/ItemInventoryClassifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInventoryClassifier : MonoBehaviour {
+
+    public string Name;
+    public string Description;
+    public int Damage;
+    public Sprite Inventoryimg;
+    public GameObject Prefab;
+    public bool CanGet;
+    public bool IsWeapon;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
