753bf070c845ea9f9e78d07253f3c216a02a8e92
landenf <landenfogle@gmail.com>
2017-10-05 07:31:24 -0500

    Added some comments and one variable to the ItemInventoryClassifier.cs

--- KoboldKing/Assets/Scripts/Inventory/ItemInventoryClassifier.cs
+++ KoboldKing/Assets/Scripts/Inventory/ItemInventoryClassifier.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class ItemInventoryClassifier : MonoBehaviour {
 
-    public string Name;
-    public string Description;
-    public int Damage;
-    public Sprite Inventoryimg;
-    public GameObject Prefab;
-    public bool CanGet;
-    public bool IsWeapon;
+    public string Name;         //Name of object
+    public string Description;  //Description of object
+    public string ItemType;     //This will help us classify if it is a weapon, coin, armor, etc.
+    public bool IsWeapon;       //This tells if it is a weapon or not, this lets us be able to assign other varibles acording to if it is a weapon.
+    public int Damage;          //If it is a weapon it will have like some sort of damage number.
+    public Sprite Inventoryimg; //This will be the image that people see in the inventory for this object.
+    public GameObject Prefab;   //This is the prefab of the object
+    public bool CanGet;         //This simply says can we pick up this object
+
+
 
 
 	void Start () {
