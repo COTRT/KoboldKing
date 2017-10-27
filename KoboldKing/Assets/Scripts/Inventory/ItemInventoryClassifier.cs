5f5e26323f6c0f4ee136fdc6b786dbc67ac3f758
landenf <landenfogle@gmail.com>
2017-10-26 19:32:18 -0500

    Edited some things and added the summary to the variables.

--- KoboldKing/Assets/Scripts/Inventory/ItemInventoryClassifier.cs
+++ KoboldKing/Assets/Scripts/Inventory/ItemInventoryClassifier.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class ItemInventoryClassifier : MonoBehaviour {
 
-    public string Name;         //Name of object
-    public string Description;  //Description of object
-    public string ItemType;     //This will help us classify if it is a weapon, coin, armor, etc.
-    public bool IsWeapon;       //This tells if it is a weapon or not, this lets us be able to assign other varibles acording to if it is a weapon.
-    public int Damage;          //If it is a weapon it will have like some sort of damage number.
-    public Sprite Inventoryimg; //This will be the image that people see in the inventory for this object.
-    public GameObject Prefab;   //This is the prefab of the object
-    public bool CanGet;         //This simply says can we pick up this object
+    /// <summary>
+    /// Name of object
+    /// </summary>
+    public string Name;
+    /// <summary>
+    /// Description of object
+    /// </summary>
+    public string Description;
+    /// <summary>
+    /// This will help us classify if it is a weapon, coin, armor, etc.
+    /// </summary>
+    public string ItemType;
+    /// <summary>
+    /// This tells if it is a weapon or not, this lets us be able to assign other varibles acording to if it is a weapon.
+    /// </summary>
+    public bool IsWeapon;
+    /// <summary>
+    /// If it is a weapon it will have like some sort of damage number.
+    /// </summary>
+    public int Damage;
+    /// <summary>
+    /// This will be the image that people see in the inventory for this object.
+    /// </summary>
+    public Sprite Inventoryimg;
+    /// <summary>
+    /// This is the prefab of the object
+    /// </summary>
+    public GameObject Prefab;
+    /// <summary>
+    /// This simply says can we pick up this object
+    /// </summary>
+    public bool CanGet;
 
 
 
 
-	void Start () {
+    void Start () {
 		
 	}
 	
