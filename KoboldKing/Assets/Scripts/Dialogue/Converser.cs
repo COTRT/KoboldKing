62d58d13f6b4564c39ff5123bf9c8fdc5dce3699
Hermanoid <lucasniewohner@gmail.com>
2018-03-23 23:13:42 +0000

    Step #2 of Dialogue: The Converser component
    I've created a component to return simple two-option response conversations (with no further conversation options from there) to fill the gap until we get the full conversation editor up and running.
    also, I put in some comments and XML documentation dinglehoppers for clarity.

---
+++ KoboldKing/Assets/Scripts/Dialogue/Converser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+//using Random = System.Random;
+
+/// <summary>
+/// This Converser implments <see cref="IConverser"/>'s <see cref="IConverser.GetDialogue"/> method by returning a random element of the dialog list
+/// </summary>
+public class Converser : MonoBehaviour, IConverser
+{
+    public List<Dialogue> Dialogues;
+
+    //Temporary fields for a simple, one-step, two-response-options conversation (for testing UI)
+    public string TempStatement;
+    public string TempResponseOption1;
+    public string TempResponseStatement1;
+    public string TempResponseOption2;
+    public string TempResponseStatement2;
+    public void Start()
+    {
+
+    }
+    public Dialogue GetDialogue()
+    {
+        //return Dialogues[new Random().Next(Dialogues.Count)];
+        return new Dialogue()
+        {
+            Statement = TempStatement,
+            Responses = new Dictionary<string, Dialogue>()
+            {
+                {TempResponseOption1, new Dialogue(TempResponseStatement1) },
+                {TempResponseOption2, new Dialogue(TempResponseStatement2) },
+            }
+        };
+    }
+}
--- KoboldKing/Assets/Scripts/Dialogue/Dialogue.cs
+++ KoboldKing/Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
+/// <summary>
+/// This class is the basis of the Dialogue system.  It's sole purpose in life is to represent a conversation, in a tree like fashion.
+/// It works pretty simply.  A given Dialogue class contains a statement, which is to be said to the player.  
+/// The dictionary of responses contains the user's potential options for responses as keys
+/// Once a user selects one of the options, the key they selected is then used to look up another Dialogue object inside that dictionary.
+/// This dialogue object will then have it's own statement (to be made by the NPC), it's own set of responses, and a variety of potential conversation paths from there...
+/// At the moment, there is no other Dialogue options to go in the Dialogue object (for example, a combat class)
+/// </summary>
 public class Dialogue  {
+    public Dialogue()
+    {
+
+    }
+    public Dialogue(string Statement)
+    {
+        this.Statement = Statement;
+    }
+    public Dialogue(string Statement, Dictionary<string,Dialogue> Responses)
+    {
+        this.Statement = Statement;
+        this.Responses = Responses;
+    }
     public string Statement { get; set; }
     public Dictionary<string,Dialogue> Responses { get; set; }
 }                                     
