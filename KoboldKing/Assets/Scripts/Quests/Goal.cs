0ac1aca45ac9123641d8010b561d138c55015fc6
Timothy Ingledue <Tim.Ingledue@fcsamerica.com>
2018-03-31 12:54:02 -0500

    #TrueUp Dev

---
+++ KoboldKing/Assets/Scripts/Quests/Goal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+
+namespace Assets.Scripts.Quests
+{
+    public class Goal
+    {
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+        public int CurrentAmount { get; set; }
+        public int RequiredAmount { get; set; }
+
+        public virtual void Init()
+        {
+            // default init stuff.
+        }
+
+
+
+        public void Evaluate()
+        {
+            if (CurrentAmount >= RequiredAmount)
+            {
+                GetCompletedStatus();
+            }
+        }
+
+        private void GetCompletedStatus()
+        {
+            Completed = true;
+        }
+    }
+}
