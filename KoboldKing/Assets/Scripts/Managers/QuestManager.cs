0fd0e5c0b0a87ae280a00cb382ed57f399599228
Timothy Ingledue <Tim.Ingledue@fcsamerica.com>
2018-04-01 18:30:02 -0500

    Completed Navagation up to Dialogue!

--- KoboldKing/Assets/Scripts/Managers/QuestManager.cs
+++ KoboldKing/Assets/Scripts/Managers/QuestManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-using Quests;
+//using Quests;
 
 namespace Assets.Scripts.Managers
 {
-    public class QuestManager
-    {
-        Goal();
-    }
+    //public class QuestManager
+    //{
+    //    Goal();
+    //}
 }
