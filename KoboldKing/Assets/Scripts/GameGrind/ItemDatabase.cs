dc71c6dd83b4c51f2e4e24d9ada0ced86f2155bd
saingledue <saingledue@outlook.com>
2018-04-12 19:34:06 -0500

    fixed minor bug in part 9

--- KoboldKing/Assets/Scripts/GameGrind/ItemDatabase.cs
+++ KoboldKing/Assets/Scripts/GameGrind/ItemDatabase.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 public class ItemDatabase : MonoBehaviour {
     public static ItemDatabase Instance { get; set; }
     private List<Item> Items { get; set; }
+    public List<BaseStat> nothing { get; set; }
     private void Start()
     {
         if(Instance != null && Instance != this)
@@ -24,16 +25,21 @@ public class ItemDatabase : MonoBehaviour {
         Debug.Log(Items[0].Stats[1].StatName + " level is " + Items[0].Stats[0].GetCalculatedStatValue());
         Debug.Log(Items[0].ItemName);
     }
+
     public Item GetItem(string itemSlug)
     {
         foreach(Item item in Items)
         {
             if (item.ObjectSlug == itemSlug)
+            {
                 return item;
-            
-            Debug.LogWarning("Couldn't find item: " + itemSlug);
-            return null;
+            }
+            else
+            {
+                Debug.LogWarning("Couldn't find item: " + itemSlug);
+            }
            
         }
+        return null;
     }
 }
