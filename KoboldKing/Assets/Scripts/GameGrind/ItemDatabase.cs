1a9ab4b55ccb3b861ec94326dfc8bab16ae184f8
Timothy Ingledue <Tim.Ingledue@fcsamerica.com>
2018-04-15 17:14:34 -0500

    #ItemDB_WIP

--- KoboldKing/Assets/Scripts/GameGrind/ItemDatabase.cs
+++ KoboldKing/Assets/Scripts/GameGrind/ItemDatabase.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
-public class ItemDatabase : MonoBehaviour {
+public class ItemDatabase : MonoBehaviour
+{
     public static ItemDatabase Instance { get; set; }
     private List<Item> Items { get; set; }
     private void Start()
     {
-        if(Instance != null && Instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
         }
@@ -24,16 +25,17 @@ public class ItemDatabase : MonoBehaviour {
         Debug.Log(Items[0].Stats[1].StatName + " level is " + Items[0].Stats[0].GetCalculatedStatValue());
         Debug.Log(Items[0].ItemName);
     }
-    public Item GetItem(string itemSlug)
-    {
-        foreach(Item item in Items)
-        {
-            if (item.ObjectSlug == itemSlug)
-                return item;
-            
-            Debug.LogWarning("Couldn't find item: " + itemSlug);
-            return null;
-           
-        }
-    }
+
+    //public Item GetItem(string itemSlug)
+    //{
+    //    foreach (Item item in Items)
+    //    {
+    //        if (item.ObjectSlug == itemSlug)
+    //            return item;
+
+    //        Debug.LogWarning("Couldn't find item: " + itemSlug);
+    //        return null;
+
+    //    }
+    //}
 }
