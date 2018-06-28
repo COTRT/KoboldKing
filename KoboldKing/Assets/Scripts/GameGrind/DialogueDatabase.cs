02adafa2a008a92fc794f07df6a476bcb5534282
saingledue <saingledue@outlook.com>
2018-06-28 14:01:07 -0500

    still working on it...

---
+++ KoboldKing/Assets/Scripts/GameGrind/DialogueDatabase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace Assets.Scripts.GameGrind
+{
+    public class DialogueDatabase : MonoBehaviour
+    {
+        public static DialogueDatabase Instance { get; set; }
+        //private List<Item> Items { get; set; }
+        private List<Dialogue> Dialogues { get; set; }
+        //public List<BaseStat> nothing { get; set; }
+        public string npc = "bob";
+       
+
+
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+            BuildDatabase();
+            GetConversation(npc);
+        }
+        private void BuildDatabase()
+        {
+            Dialogues = JsonConvert.DeserializeObject<List<Dialogue>>(Resources.Load<TextAsset>("GameGrind/JSON/DialogueTest").ToString());
+            //Debug.Log(Items[0].Stats[1].StatName + " level is " + Items[0].Stats[0].GetCalculatedStatValue());
+            Debug.Log(Dialogues[0].Introduction);
+        }
+
+        public Dialogue GetConversation(string Dialoguesv)
+        {
+            // TODO: Lamba expression with where clause could be faster?
+            foreach (Dialogue talk in Dialogues)
+            {
+                if (talk.Name == Dialoguesv)
+                    return talk;
+            }
+
+            Debug.LogWarning("Couldn't find conversation " + Dialoguesv);
+            return null;
+
+        }
+    }
+}
