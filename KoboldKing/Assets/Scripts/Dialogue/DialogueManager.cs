eb5c7660a5dbb523b9ee85bc84399b004eeb58ab
landenf <landenfogle@gmail.com>
2018-06-28 20:27:15 -0500

    Major WIP

---
+++ KoboldKing/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class DialogueManager
+{
+    static Dictionary<string, ObjectDialogueTrees> _masterDialogueDict;
+
+
+    public static Dictionary<string, ObjectDialogueTrees> MasterDialogueDict
+    {
+        get
+        {
+            return _masterDialogueDict ?? (_masterDialogueDict = GetJSONDatabase());
+        }
+
+        set
+        {
+            _masterDialogueDict = value;
+        }
+    }
+    
+    /// <summary>
+    /// This returns a dictionary mapping GameObject names to their corresonding Dialogue trees, which the object itself can then choose from 
+    /// depending on the environment (say, follow the "Quest Completed" tree after a quest is completed)
+    /// </summary>
+    /// <returns></returns>
+    private static Dictionary<string,ObjectDialogueTrees> GetJSONDatabase()
+    {
+        return JsonConvert.DeserializeObject<Dictionary<string, ObjectDialogueTrees>>(Resources.Load<TextAsset>("GameGrind/JSON/DialogueTest").ToString());
+
+    }
+}
---
+++ KoboldKing/Assets/Scripts/Dialogue/PlayerDialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDialogueManager : MonoBehaviour {
+
+    private Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+       mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("EnterDialogue"))
+        {
+            RaycastHit hit;
+            Ray raycast = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(raycast,out hit))
+            {
+                IConverser converser = hit.transform.gameObject.GetComponent<IConverser>();
+                if (converser != null)
+                {
+                    Debug.Log(converser.GetDialogue().Statement);
+                }
+            }
+        }
+	}
+}
