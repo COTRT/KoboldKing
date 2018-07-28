323ba685ccca00ba2e5f20a388fb31f05c5ea36b
landenf <landenfogle@gmail.com>
2018-07-28 13:56:17 -0500

    I created a simple text transitioning cut scene wiith the text he gave me. If you look at It it is pretty boring becuase I dont have the best artistic mind If someone had an Idea I could implement please tell me.

---
+++ KoboldKing/Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour {
+
+	public float delay = 0.1f;
+	private string fullText;
+	private string currentText = "";
+    public Text TextObject;
+    public float WaitTime;
+	// Use this for initialization
+	void Start () {
+        fullText = TextObject.text;
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText(){
+        yield return new WaitForSeconds(WaitTime);
+        for (int i = 0; i < fullText.Length; i++){
+            currentText = fullText.Substring(0,i);
+			this.GetComponent<Text>().text = currentText;
+			yield return new WaitForSeconds(delay);
+		}
+	}
+}
